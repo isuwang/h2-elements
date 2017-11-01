839ed97ee38f62cf2df85ef7515fa1095d791f0f
WahsonLeung <hsliang1@163.com>
2017-11-01 17:55:34 +0800

    Hide private variables & rename mock flag.

--- utils/mock_check.js
+++ utils/mock_check.js
@@ -1,20 +1,13 @@
-console.log("mock_check");
-const mockSearchParam = window.location.search.replace("?", "").split("&").map(part => part.split("="))
-  .reduce((tmp, part) => Object.assign(tmp, {[part[0]]: part[1]}), {});
-
-const mockDataSrc = mockSearchParam["mock"];
-
-window.mockEnabled = !!mockDataSrc;
-
-let loaded = false;
-if (mockDataSrc && typeof MockDataPool === "undefined") {
-  const scriptTag = document.createElement("script");
-  scriptTag.type = "text/javascript";
-  scriptTag.src = mockDataSrc;
-  scriptTag.onload = () => {
-    loaded = true;
-  };
-  document.head.appendChild(scriptTag);
+(function () {
+  const mockSearchParam = window.location.search.replace("?", "").split("&").map(part => part.split("="))
+    .reduce((tmp, part) => Object.assign(tmp, {[part[0]]: part[1]}), {});
   
-}
+  const mockDataSrc = mockSearchParam["mock"];
+  
+  window.__mockEnabled = !!mockDataSrc;
+  
+  if (mockDataSrc) {
+    document.write(`<script type="text/javascript" src="${mockDataSrc}"></script>`)
+  }
+})();
 
