a325e87208e1014251c8a092c1746415e9b502a5
WahsonLeung <hsliang1@163.com>
2017-11-02 15:06:56 +0800

    Some adjustments.

--- demo/h2-fetch/mockData.js
+++ demo/h2-fetch/mockData.js
@@ -1,58 +1,17 @@
-const MockDataPool = (function () {
-  
-  const mockDatas = {};
-  
-  const calHash = function (method, url, headersArr) {
-    const headersHash = headersArr.sort()
-      .map(([headerKey, headerVal]) => `${headerKey}:${headerVal}`).join(";");
-    return `[${method}]${url}##${headersHash}`.toLowerCase();
-  };
-  
-  return {
-    match({url, method, headers = {}} = {}) {
-      
-      if (typeof headers !== "object") throw new TypeError("headers must be an object");
-      
-      const headersArr = [];
-      
-      // make sure headers is an instance of Headers
-      if (Headers.prototype.isPrototypeOf(headers)) {
-        for (let h of headers.entries()) {
-          headersArr.push(h);
-        }
-      }
-      
-      const hash = calHash(method, url, headersArr);
-      return mockDatas[hash] && mockDatas[hash].response || null;
-    },
-    
-    when(method, url) {
-      // fix url
-      const fixedUrl = new URL(url, window.location.href).href;
-      const headers = [];
-      
-      return {
-        withExpectedHeader(key, value) {
-          headers.push([key, value]);
-          return this;
-        },
-        responseWith(response) {
-          let hash = calHash(method, fixedUrl, headers);
-          mockDatas[hash] = {request: {url: fixedUrl, method, headers}, response};
-        }
-      };
-    }
-  }
-})();
-
 // test
 // init first mock
-MockDataPool.when("POST", "/page/to/index.do")
+MockDataPool.when("POST", "/path/to/index.do")
   .withExpectedHeader("content-type", "application/json;charset=utf-8")
   .withExpectedHeader("Cache-Control", "no-cache")
   .responseWith({status: 200, body: JSON.stringify({a: 1, b: 2})});
 
-MockDataPool.when("POST", "/page/to/index.do")
+MockDataPool.when("POST", "/path/to/index.do")
   .withExpectedHeader("Cache-Control", "no-cache")
   .responseWith({status: 200, body: JSON.stringify({a: 1, b: 2, c: 3})});
-  
+
+MockDataPool.when("POST", "/path/to/index.do")
+  .withExpectedHeader("test-header", "no-value")
+  .responseWith({status: 200, body: "Fuck the world if you are rick, otherwise fuck yourself!"});
+
+MockDataPool.when("GET", "/path/to/helloworld.do")
+  .responseWith({status: 200, body: "达则兼济天下,穷则独善其身"});
---
+++ utils/mock_setup.js
@@ -0,0 +1,64 @@
+(function (global) {
+  
+  const mockSearchParam = window.location.search.replace("?", "").split("&").map(part => part.split("="))
+    .reduce((tmp, part) => Object.assign(tmp, {[part[0]]: part[1]}), {});
+  
+  const mockDataSrc = mockSearchParam["mock"];
+  
+  global.__mockEnabled = !!mockDataSrc;
+  
+  if (mockDataSrc) {
+    // instance MockDataPool
+    const MockDataPool = (function () {
+      const mockDatas = {};
+    
+      const calHash = function (method, url, headersArr) {
+        const headersHash = headersArr.sort()
+          .map(([headerKey, headerVal]) => `${headerKey}:${headerVal}`).join(";");
+        return `[${method}]${url}##${headersHash}`.toLowerCase();
+      };
+    
+      return {
+        match({url, method, headers = {}} = {}) {
+        
+          if (typeof headers !== "object") throw new TypeError("headers must be an object");
+        
+          const headersArr = [];
+        
+          // make sure headers is an instance of Headers
+          if (Headers.prototype.isPrototypeOf(headers)) {
+            for (let h of headers.entries()) {
+              headersArr.push(h);
+            }
+          }
+        
+          const hash = calHash(method, url, headersArr);
+          return mockDatas[hash] && mockDatas[hash].response || null;
+        },
+      
+        when(method, url) {
+          // fix url
+          const fixedUrl = new URL(url, window.location.href).href;
+          const headers = [];
+        
+          return {
+            withExpectedHeader(key, value) {
+              headers.push([key, value]);
+              return this;
+            },
+            responseWith(response) {
+              let hash = calHash(method, fixedUrl, headers);
+              mockDatas[hash] = {request: {url: fixedUrl, method, headers}, response};
+            }
+          };
+        }
+      }
+    })();
+  
+    window.MockDataPool = MockDataPool;
+    // load init data
+    document.write(`<script type="text/javascript" src="${mockDataSrc}"></script>`)
+  }
+  
+})(window);
+
