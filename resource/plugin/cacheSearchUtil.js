4cfb5179635af4f10b0aef98c64c8fe24a513af4
linyuchi <linyuchi@iplas.com.cn>
2017-11-09 16:34:51 +0800

    组件优化 pinyinUtil.js开发 cacheSearchUtil.js开发 拼音字库提交

---
+++ resource/plugin/cacheSearchUtil.js
@@ -0,0 +1,54 @@
+/**
+ * 缓存搜索的插件
+ *
+ * 缓存数据结构_cacheItems:
+ * [{item: {label:"快塑网",business:"塑料"}, searches: ["快塑网","KUAISUWANG","KSW","塑料","SULIAO","SL"],matchCount:0},
+ *  {item: {label:"京东",business:"电商"}, searches: ["京东","JINGDONG","JD","电商","DIANSHANG","DS"],matchCount:0 }]
+ *
+ */
+class CacheSearchUtil {
+    constructor() {
+        this.resetCache();
+    }
+
+    /**
+     * 查询
+     * @param keyword 关键字
+     * @returns {Array.<*>}
+     */
+    search(keyword) {
+        let parts = keyword.toUpperCase().trim().split(",");
+        /**
+         * 匹配的结果集
+         * 1.filter操作：将关键字匹配的cacheItem过滤出来，得到匹配缓存数组
+         * 2.map操作：将匹配的缓存项的匹配计数器matchCount加1
+         * 3.sort操作：通过匹配计数器matchCount按从大到小对结果集排序
+         * 4.map操作：返回pojo数组
+         */
+        return this._cacheItems.filter(
+            cacheItem => parts.every(part => cacheItem.searches.some(s => s.indexOf(part) > -1))
+        ).map(cacheItem => {
+            cacheItem.matchCount++;
+            return cacheItem
+        }).sort((prev, next) => next.matchCount - prev.matchCount
+        ).map(
+            cacheItem => cacheItem.item
+        );
+    }
+
+    /**
+     * 新增缓存项
+     * @param item 缓存对象POJO 如:{label:"快塑网",business:"塑料"}
+     * @param searches 缓存对象搜索关键字符串数组，包含了item的特定字段值，特定字段的全拼、简拼字符串,如：["快塑网","KUAISUWANG","KSW","塑料","SULIAO","SL"]
+     */
+    addCacheItem(item, searches) {
+        this._cacheItems.push({"item": item, "searches": searches, matchCount: 0});
+    }
+
+    /**
+     * 重置索引缓存
+     */
+    resetCache() {
+        this._cacheItems = [];
+    }
+}
---
+++ resource/plugin/pinyinUtil.js
@@ -0,0 +1,117 @@
+/**
+ * Created by SkyLin on 2017/11/8.
+ */
+class PinyinUtil {
+    constructor() {
+        /**
+         * 无加载字典文件的时候抛错提示
+         */
+        if (!window.firstLetterDictionary) {
+            console.error("请先加载首字母拼音字典firstLetterDictionary.js");
+            return;
+        }
+        if (!window.commonDictionary) {
+            console.error("请先加载通用拼音字典commonDictionary.js");
+            return;
+        }
+
+        /**
+         * 初始化通用字典和拼音首字母字典
+         */
+        let commonDictionary = {};
+        let commonDict = window.commonDictionary;
+        let pinyinArr = commonDict.split(',');
+        for (let i = 0, len = pinyinArr.length; i < len; i++) {
+            // 这段代码耗时28毫秒左右，对性能影响不大，所以一次性处理完毕
+            commonDictionary[String.fromCharCode(i + 19968)] = pinyinArr[i];
+        }
+        this.commonDictionary = commonDictionary;
+        this.firstLetterDictionary = window.firstLetterDictionary;
+    }
+
+    /**
+     * 汉字转全拼
+     * @param userInput 用户输入关键字(允许输入英文字母和中文)
+     * @returns {*}
+     */
+    converToCompletePinyin(userInput) {
+        /**
+         * 过滤words中的所有非中文非字母的字符
+         */
+        let keyword = (userInput + "").trim().replace(/[^(a-zA-Z\u4e00-\u9fa5)]/g, "");
+        if (!keyword) {
+            return [];
+        }
+
+        let dealtWords = keyword.split("");
+        let reg = /[a-zA-Z]/;
+        let results = dealtWords.map(word => {
+            return reg.test(word) ? word : this.commonDictionary[word]
+        });
+        return this._handlePolyphone(results, " ");
+    }
+
+    /**
+     * 汉字转拼音首字母
+     * @param userInput 用户输入关键字(允许输入英文字母和中文)
+     * @returns {*}
+     */
+    converToFirstLetterPinyin(userInput) {
+        /**
+         * 过滤words中的所有非中文非字母的字符
+         */
+        let keyword = (userInput + "").trim().replace(/[^(a-zA-Z\u4e00-\u9fa5)]/g, "");
+        if (!keyword) {
+            return [];
+        }
+
+        let result = [];
+        for (let i = 0; i < keyword.length; i++) {
+            let ch = keyword.charAt(i);
+            /**
+             * 字母不转换
+             */
+            if (/a-zA-Z/.test(ch)) {
+                result.push(ch);
+                continue;
+            }
+
+            /**
+             * 在首字母拼音字典中查找，优先查找多音字，否则查找所有字
+             */
+            let unicode = keyword.charCodeAt(i);
+            if (unicode >= 19968 && unicode <= 40869) {
+                ch = this.firstLetterDictionary.polyphone[unicode] || this.firstLetterDictionary.all.charAt(unicode - 19968);
+            }
+            result.push(ch);
+        }
+        return this._handlePolyphone(result, "");
+    }
+
+    /**
+     * 处理多音字，并去重
+     * 1.['hen', 'xing heng xing hang'] 转换成 ['hen xing', 'hen heng', 'hen hang']
+     * 2.['D', 'ZCZ', 'F']转换成['DZF', 'DCF']
+     * @param arr 字符串数组
+     * @param splitter 字符串分隔符
+     * @returns {[string]}
+     * @private
+     */
+    _handlePolyphone(arr, splitter) {
+        splitter = splitter || "";
+        // 对数组的字符串元素去重,封装成字符串二维数组
+        let array = arr.map(e => Array.from(new Set(e.split(splitter))));
+
+        let result = [''], temp = [];
+        for (let i = 0; i < array.length; i++) {
+            temp = [];
+            let t = array[i];
+            for (let j = 0; j < t.length; j++) {
+                for (let k = 0; k < result.length; k++)
+                    temp.push(result[k] + "" + t[j]);
+            }
+            result = temp;
+        }
+        return result.map(e => e.toUpperCase());
+    }
+}
