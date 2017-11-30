81679ca98c64b36ac3694b8fff98d23409937877
linyuchi <linyuchi@iplas.com.cn>
2017-11-30 14:51:22 +0800

    mock模拟数据引入方式使用import

--- demo/h2-fetch/mockData.js
+++ demo/h2-fetch/mockData.js
@@ -1,17 +1,16 @@
-// test
-// init first mock
+import {testObj, testObj1, helloStr, testStr} from "./data.js";
 MockDataPool.when("POST", "/path/to/index.do")
-  .withExpectedHeader("content-type", "application/json;charset=utf-8")
-  .withExpectedHeader("Cache-Control", "no-cache")
-  .responseWith({status: 200, body: JSON.stringify({a: 1, b: 2})});
+    .withExpectedHeader("content-type", "application/json;charset=utf-8")
+    .withExpectedHeader("Cache-Control", "no-cache")
+    .responseWith({status: 200, body: JSON.stringify(testObj)});
 
 MockDataPool.when("POST", "/path/to/index.do")
-  .withExpectedHeader("Cache-Control", "no-cache")
-  .responseWith({status: 200, body: JSON.stringify({a: 1, b: 2, c: 3})});
+    .withExpectedHeader("Cache-Control", "no-cache")
+    .responseWith({status: 200, body: JSON.stringify(testObj1)});
 
 MockDataPool.when("POST", "/path/to/index.do")
-  .withExpectedHeader("test-header", "no-value")
-  .responseWith({status: 200, body: "Fuck the world if you are rick, otherwise fuck yourself!"});
+    .withExpectedHeader("test-header", "no-value")
+    .responseWith({status: 200, body: helloStr});
 
 MockDataPool.when("GET", "/path/to/helloworld.do")
-  .responseWith({status: 200, body: "达则兼济天下,穷则独善其身"});
+    .responseWith({status: 200, body: testStr});
