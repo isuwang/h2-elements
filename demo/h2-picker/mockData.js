d44667f0914eb3da7a70fd6659070505efd94cdc
WahsonLeung <hsliang1@163.com>
2018-01-11 15:46:46 +0800

    Fixed demos.

--- demo/h2-picker/mockData.js
+++ demo/h2-picker/mockData.js
@@ -1,13 +1,17 @@
 import {banks, companies} from "./data.js";
 
 MockDataPool.when("GET", "/init.do")
-    .responseWith({
-        status: 200,
-        body: JSON.stringify(banks)
-    });
-
+  .responseWith({
+    status: 200,
+    body: JSON.stringify(banks)
+  });
 
 MockDataPool.when("GET", "/query.do")
-    .responseWith({
-        status: 200, body: JSON.stringify(companies)
-    });
+  .responseWith({
+    status: 200, body: JSON.stringify(companies)
+  });
+
+MockDataPool.when("GET", "/more_banks.do")
+  .responseWith({
+    status: 200, body: JSON.stringify(companies)
+  });
